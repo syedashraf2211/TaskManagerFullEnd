4e14874a0940685eec0de1fc0317699fe76184e3
keerthi-chamarthi <keerthichamarthi99@gmail.com>
2021-11-18 14:49:19 +0530

    With springsecurity working

--- TaskManager/src/main/java/com/training/TaskManager/HomeController.java
+++ TaskManager/src/main/java/com/training/TaskManager/HomeController.java
@@ -22,20 +22,9 @@ import com.training.TaskManager.service.ManagerService;
 import com.training.TaskManager.service.TaskService;
 
 @Controller
-//@SessionAttributes({"mmail"})
-//@RequestMapping("/mng")
 public class HomeController 
 {
 	
-	@Autowired
-	EmployeeService eservice;
-	
-	@Autowired
-	ManagerService mservice;
-	
-	@Autowired
-	TaskService tservice;
-
 	@RequestMapping("/")
 	public String home()
 	{
@@ -53,117 +42,5 @@ public class HomeController
 	{
 		return "mlogin";
 	}
-	/*
-	@RequestMapping("/mvalidate")
-	public String validateManager(Model m)
-	{
-		List<TaskInfo> tinfo= tservice.getAllTasks();
-		//ManagerInfo mnginfo = mservice.findByEmail("mmail");
-		//System.out.println(mnginfo.getMid());
-		m.addAttribute("tinfo",tinfo);
-		return "mhome";
-	}
-	/*
-	@PostMapping("/mvalidate")
-	public String validateManager(@RequestParam String mmail,Model m)
-	{
-		//System.out.println(mmail);
-		List<TaskInfo> tinfo= tservice.getAllTasks();
-		m.addAttribute("tinfo",tinfo);
-		m.addAttribute("mmail", mmail);
-		return "mhome";
-	}
 	
-	
-	@PostMapping("/mvalidate")
-	public String postManager(Model m,@RequestParam String email)
-	{
-		//System.out.println(minfo.getEmail());
-		List<TaskInfo> tinfo= tservice.getAllTasks();
-		m.addAttribute("tinfo",tinfo);
-		m.addAttribute("mmail",email);
-		return "mhome";
-	}
-	
-	@RequestMapping("/mregister")
-	public String createManager()
-	{
-		return "mregistration";
-	}
-	/*
-	@RequestMapping("/eregister")
-	public String registerEmployee()
-	{
-		return "eregistration";
-	}
-	
-	@RequestMapping("/ecreate")
-	public String createEmployee(@ModelAttribute EmployeeInfo einfo)
-	{
-		//System.out.println(einfo.getEname());
-		eservice.saveOrUpdate(einfo);
-		return "elogin";
-	}
-	
-	@RequestMapping("/mcreate")
-	public String createManager(@ModelAttribute ManagerInfo minfo)
-	{
-		//System.out.println(minfo.getMname());
-		mservice.saveOrUpdate(minfo);
-		return "mlogin";
-	}
-	
-	@RequestMapping("/tregister")
-	public String registerTask(Model m)
-	{
-		List<EmployeeInfo> empdata = eservice.getAllEmployees();
-		//System.out.println(empdata.get(0).getEname());
-		m.addAttribute("empdata", empdata);
-		return "tcreate";
-	}
-	
-	@RequestMapping("/createtask")
-	public String createTask(@ModelAttribute TaskInfo tinfo,@RequestParam String Enddate,@RequestParam String email,@RequestParam String AssignedBy) throws Exception
-	{
-		//  System.out.println(AssignedBy);
-		tservice.saveOrUpdate(tinfo, Enddate,email,AssignedBy);
-		return "redirect:/mng/mvalidate";
-	}
-	/*
-	@RequestMapping("/ecreatetask")
-	public String ecreateTask(@ModelAttribute TaskInfo tinfo) throws Exception
-	{
-		System.out.println(tinfo.getProgress());
-		tservice.updateProgress(tinfo);
-		return "redirect:/evalidate";
-	}
-	
-	@RequestMapping(value = "/updatetask/{taskId:[\\d]+}",method = RequestMethod.GET)
-	public String updateTask(@PathVariable("taskId") int tid,Model m) throws Exception
-	{
-		//System.out.println(tid);
-		TaskInfo tinfo = tservice.getTask(tid);
-		m.addAttribute("tinfo",tinfo);
-		return "utask";
-	}
-	/*
-	@RequestMapping(value = "/eupdatetask/{taskId:[\\d]+}",method = RequestMethod.GET)
-	public String eupdateTask(@PathVariable("taskId") int tid,Model m) throws Exception
-	{
-		//System.out.println(tid);
-		
-		TaskInfo tinfo = tservice.getTask(tid);
-		m.addAttribute("tinfo",tinfo);
-		return "eutask";
-	}
-	
-	@RequestMapping(value = "/deletetask/{taskId:[\\d]+}",method = RequestMethod.GET)
-	public String deleteTask(@PathVariable("taskId") int tid,Model m) throws Exception
-	{
-		//System.out.println(tid);
-		tservice.deleteTask(tid);
-		
-		return "redirect:/mng/mvalidate";
-	}
-	*/
 }
